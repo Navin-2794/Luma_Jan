c1d9c0b25cfcdf986be4e0d04dc5b982d7a702b6
Navin-2794 <navinkumarvraj@gmail.com>
2025-01-25 11:42:20 +0530

    INitial Commit

---
+++ src/test/java/com/Luma_IPT_Jan/AppTest.java
@@ -0,0 +1,13 @@
+package com.Luma_IPT_Jan;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
